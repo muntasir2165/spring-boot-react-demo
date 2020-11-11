eb64f4f7717a86a54e653c3585b8e01340c083f4
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-11 13:18:42 -0700

    v-5 creating endpoint to retrieve students

---
+++ src/main/java/com/amigoscode/demo/student/StudentController.java
@@ -0,0 +1,21 @@
+package com.amigoscode.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(new Student(UUID.randomUUID(), "James", "Bond", "jamesbond@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(), "Elisa", "Tamara", "elisatamara@hotmail.com", Student.Gender.FEMALE));
+    }
+
+
+}
