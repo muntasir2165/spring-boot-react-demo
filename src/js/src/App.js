0c1f772e0914a44bd7ff97699a501b1075060e2a
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-12 16:30:22 -0700

    v-9 adding react state

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
 
-function App() {
-  getAllStudents().then((res) =>
-    res.json().then((students) => {
-      console.log(students);
-    })
-  );
-  return <h1>Hello World Spring Boot & React</h1>;
+class App extends Component {
+  state = {
+    students: [],
+  };
+
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
+    getAllStudents().then((res) =>
+      res.json().then((students) => {
+        console.log(students);
+        this.setState({ students });
+      })
+    );
+  };
+
+  render() {
+    const { students } = this.state;
+
+    if (students && students.length) {
+      return students.map((student, index) => (
+        <div key={index}>
+          <h2>{student.studentId}</h2>
+          <p>{student.firstName}</p>
+          <p>{student.lastName}</p>
+          <p>{student.gender}</p>
+          <p>{student.email}</p>
+        </div>
+      ));
+    }
+
+    return <h1>No Students Found</h1>;
+  }
 }
 
 export default App;
