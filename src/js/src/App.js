c9b7639a6d17df201cf00572e0c6d1be848b241f
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-12 15:09:26 -0700

    v-8 GET request from frontend to backend

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { getAllStudents } from './client';
 
 function App() {
+  getAllStudents().then((res) =>
+    res.json().then((students) => {
+      console.log(students);
+    })
+  );
   return <h1>Hello World Spring Boot & React</h1>;
 }
 
