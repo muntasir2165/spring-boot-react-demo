22ac478d36ce4a1c83ddc74e1c8e417a5991c0ca
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-12 20:16:41 -0700

    v-11 antd table component

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
+import { Table } from 'antd';
 
 class App extends Component {
   state = {
@@ -24,15 +25,37 @@ class App extends Component {
     const { students } = this.state;
 
     if (students && students.length) {
-      return students.map((student, index) => (
-        <div key={index}>
-          <h2>{student.studentId}</h2>
-          <p>{student.firstName}</p>
-          <p>{student.lastName}</p>
-          <p>{student.gender}</p>
-          <p>{student.email}</p>
-        </div>
-      ));
+      const columns = [
+        {
+          title: 'Student Id',
+          dataIndex: 'studentId',
+          key: 'studentId',
+        },
+        {
+          title: 'First Name',
+          dataIndex: 'firstName',
+          key: 'firstName',
+        },
+        {
+          title: 'Last Name',
+          dataIndex: 'lastName',
+          key: 'lastName',
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email',
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender',
+        },
+      ];
+
+      return (
+        <Table dataSource={students} columns={columns} rowKey='studentId' />
+      );
     }
 
     return <h1>No Students Found</h1>;
