89c2df265cc06529913875601432be328c54b889
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-15 02:08:44 -0700

    v-26-27-28 jdbc template and result sets; select statement; row mappers

--- src/main/java/com/amigoscode/demo/student/StudentDataAccessService.java
+++ src/main/java/com/amigoscode/demo/student/StudentDataAccessService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,10 +10,25 @@ import java.util.UUID;
 @Repository
 public class StudentDataAccessService {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public List<Student> selectAllStudents() {
-        return List.of(
-                new Student(UUID.randomUUID(), "James", "Bond", "jamesbond@gmail.com", Student.Gender.MALE),
-                new Student(UUID.randomUUID(), "Elisa", "Tamara", "elisatamara@hotmail.com", Student.Gender.FEMALE)
-        );
+        String sql = "SELECT student_id, first_name, last_name, email, gender FROM student";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            String studentIdStr = resultSet.getString("student_id");
+            UUID studentId = UUID.fromString(studentIdStr);
+
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+            String genderStr = resultSet.getString("gender").toUpperCase();
+            Student.Gender gender = Student.Gender.valueOf(genderStr);
+            return new Student(studentId, firstName, lastName, email, gender);
+        });
     }
 }
