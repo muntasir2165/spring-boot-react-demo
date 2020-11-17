13598aa4136ee4afdb4fdb9ada343a9e80ad60b6
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-17 02:17:30 -0700

    v-38 POST mapping (server side)

--- src/main/java/com/amigoscode/demo/student/StudentController.java
+++ src/main/java/com/amigoscode/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.amigoscode.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,8 @@ public class StudentController {
         return studentService.getAllStudents();
     }
 
-
+    @PostMapping
+    public void addNewStudent(@RequestBody Student student) {
+        System.out.println(student);
+    }
 }
